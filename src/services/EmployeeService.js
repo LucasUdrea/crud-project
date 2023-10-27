5394dfbba64e915df3cc4a166cdb59c17a6064d2
Lucas Udrea <lucasudrea@gmail.com>
2023-10-27 15:57:21 -0300

    Crear empleado terminado

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -7,6 +7,10 @@ class employeeService{
     getEmployees(){
         return axios.get(EMPLOYEE_API_BASE_URL);
     }
+
+    createEmployee(employee){
+        return axios.post(EMPLOYEE_API_BASE_URL, employee);
+    }
 }
 
 export default new employeeService()
