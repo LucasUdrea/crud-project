7069d3248373783444d8f1d62a8ac5680087ccb6
Lucas Udrea <lucasudrea@gmail.com>
2023-10-27 02:28:16 -0300

    Obtencion de lista, proceso de hacer comp de crear

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+
+class FooterComponent extends Component {
+    constructor(props){
+        super(props)
+    
+        this.state = {
+    
+        }
+       }
+    
+    render() {
+        return (
+            <div>
+                <footer className='footer'>
+                    <span className='text-muted'>Made by Lucas Udrea</span>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default FooterComponent;
