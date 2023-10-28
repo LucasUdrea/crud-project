31360b5967b8d4666aa7516bbe022dc9b34085df
Lucas Udrea <lucasudrea@gmail.com>
2023-10-28 19:43:39 -0300

    minor changes

--- springboot-backend/src/main/java/crudprojectbackend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/crudprojectbackend/controller/EmployeeController.java
@@ -9,30 +9,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import crudprojectbackend.model.Employee;
 import crudprojectbackend.repository.EmployeeRepository;
+
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
-	
-	//get all employees
+
+	// get all employees
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployees(){
+	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
-		
-	//create employee rest api
+
+	// create employee rest api
 	@PostMapping("/employees")
-	public Employee createEmployee (@RequestBody Employee employee) {
+	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
 	}
-		
-	
 
 }
